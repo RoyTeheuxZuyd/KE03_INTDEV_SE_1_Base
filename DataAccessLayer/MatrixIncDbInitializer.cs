c99cdfc636c13aa2971cbe0ef9cb53e93ef9abef
RoyTeheuxZuyd <2506589teheux@zuyd.nl>
2026-06-25 20:17:09 +0200

    meer sample products + tweak homepage

--- DataAccessLayer/MatrixIncDbInitializer.cs
+++ DataAccessLayer/MatrixIncDbInitializer.cs
@@ -17,35 +17,13 @@ namespace DataAccessLayer
                 return;   // DB has been seeded
             }
 
-            // TODO: Hier moet ik nog wat namen verzinnen die betrekking hebben op de matrix.
-            // - Denk aan de m3 boutjes, moertjes en ringetjes.
-            // - Denk aan namen van schepen
-            // - Denk aan namen van vliegtuigen
-            
-            //Deactive for now due to the order history page which is still w.i.p and displaying these incorrectly. 
-            //var customers = new Customer[]
-            //{
-            //    new Customer { Name = "Neo", Address = "123 Elm St" , Active=true},
-            //    new Customer { Name = "Morpheus", Address = "456 Oak St", Active = true },
-            //    new Customer { Name = "Trinity", Address = "789 Pine St", Active = true }
-            //};
-            //context.Customers.AddRange(customers);
-
-            //var orders = new Order[]
-            //{
-            //    new Order { Customer = customers[0], OrderDate = DateTime.Parse("2021-01-01")},
-            //    new Order { Customer = customers[0], OrderDate = DateTime.Parse("2021-02-01")},
-            //    new Order { Customer = customers[1], OrderDate = DateTime.Parse("2021-02-01")},
-            //    new Order { Customer = customers[2], OrderDate = DateTime.Parse("2021-03-01")}
-            //};
-            //context.Orders.AddRange(orders);
-
             var parts = new Part[]
             {
                 new Part { Name = "Tandwiel", Description = "Overdracht van rotatie in bijvoorbeeld de motor of luikmechanismen"},
                 new Part { Name = "M5 Boutje", Description = "Bevestiging van panelen, buizen of interne modules"},
                 new Part { Name = "Hydraulische cilinder", Description = "Openen/sluiten van zware luchtsluizen of bewegende onderdelen"},
-                new Part { Name = "Koelvloeistofpomp", Description = "Koeling van de motor of elektronische systemen."}
+                new Part { Name = "Koelvloeistofpomp", Description = "Koeling van de motor of elektronische systemen."},
+                new Part { Name = "Brillendoekje", Description = "Voor het schoonhouden van brillenglazen."}
             };
             context.Parts.AddRange(parts);
 
@@ -53,12 +31,16 @@ namespace DataAccessLayer
             {
                 new Product { Name = "Nebuchadnezzar", Description = "Het schip waarop Neo voor het eerst de echte wereld leert kennen", Price = 10000.00m, ProductImageName = "nebuchadnezzar.jpg"},
                 new Product { Name = "Jack-in Chair", Description = "Stoel met een rugsteun en metalen armen waarin mensen zitten om ingeplugd te worden in de Matrix via een kabel in de nekpoort", Price = 500.50m, ProductImageName = "jackinchair.jpg"},
-                new Product { Name = "EMP (Electro-Magnetic Pulse) Device", Description = "Wapentuig op de schepen van Zion", Price = 129.99m, ProductImageName = "empdevice.jpg" }
+                new Product { Name = "EMP (Electro-Magnetic Pulse) Device", Description = "Wapentuig op de schepen van Zion", Price = 129.99m, ProductImageName = "empdevice.jpg" },
+                new Product { Name = "Agent Smith sunglasses", Description = "Speciale zonnebril voor geheime operaties.", Price = 9.99m, ProductImageName = "agentsmithglasses.png"}
             };
 
+            products[0].Parts.Add(parts[0]);
+            products[0].Parts.Add(parts[3]);
             products[1].Parts.Add(parts[1]);
             products[2].Parts.Add(parts[0]);
             products[2].Parts.Add(parts[3]);
+            products[3].Parts.Add(parts[4]);
 
             context.Products.AddRange(products);
 
