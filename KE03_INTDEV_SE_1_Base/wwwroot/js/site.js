f8c4485c1d60a2a6e945d9b23ec8a6fa3d5ac464
RoyTeheuxZuyd <2506589teheux@zuyd.nl>
2026-07-02 10:24:17 +0200

    js functie profiel icon met placeholder lofi
    geen tijd meer gehad om deze pages te implementeren maar wou ze wel als concepts in de applicatie tonen voor beeldvorming

--- KE03_INTDEV_SE_1_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_1_Base/wwwroot/js/site.js
@@ -3,7 +3,6 @@
 
 // Write your JavaScript code.
 
-
 //search bar clearer
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -18,5 +17,47 @@ document.addEventListener("DOMContentLoaded", function () {
             form.submit();
         }
     });
-
 });
+
+//profile icon dropdown
+//wait until full html loaded until we retrieve elements
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("JS loaded");
+
+    const profileButton = document.getElementById("profileButton");
+    const profileMenu = document.getElementById("profileMenu");
+    console.log("button:", profileButton);
+    console.log("menu:", profileMenu);
+
+    // toggle open and close menu from the icon
+    function toggleMenu(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        profileMenu.classList.toggle("open");
+    }
+
+    // open or close menu when clicking icon
+    profileButton.addEventListener("click", toggleMenu);
+
+    // when interact outside icon close menu
+    document.addEventListener("click", function (e) {
+        if (!profileMenu.contains(e.target) &&
+            e.target !== profileButton) {
+
+            profileMenu.classList.remove("open");
+        }
+    });
+});
+
+//logout conceptual
+const logoutButton = document.getElementById("logoutButton");
+
+if (logoutButton) {
+    logoutButton.addEventListener("click", function (e) {
+        e.preventDefault(); // <-- DIT is wat de # en navigatie stopt
+        e.stopPropagation();
+
+        alert("Je bent uitgelogd");
+    });
+}
